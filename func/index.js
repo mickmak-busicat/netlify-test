c0d7382e61ff41b31a540c1255c776a215507ad7
Mick Mak <mk.triniti@gmail.com>
2020-09-10 17:46:35 +0800

    Test first api video

--- func/index.js
+++ func/index.js
@@ -1,6 +1,22 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
+  const { token } = event;
+  console.log(event);
+
+  if (token === 'ABCD') {
+    callback(null, {
+      statusCode: 307,
+      headers: {
+        location: [
+          /** 'AWS_S3_ENDPOINT' will be packed by webpack */
+          /* eslint-disable-next-line no-undef */
+          { key: 'Location', value: 'https://elated-joliot-752915.netlify.app/files/5_6330189367990026289.mp4' },
+        ],
+      },
+    });
+  } else {
+    callback(null, {
+      statusCode: 200,
+      body: 'no token',
+    });
+  }
 };
